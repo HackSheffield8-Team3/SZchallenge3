cfb89e1efedf2170c9e2006ec0edd167a5741ce4
bwsmith-dev <bwsmith161@gmail.com>
2023-11-11 22:08:05 +0000

    "Inserted missing self(s)"

--- hydro_model.py
+++ hydro_model.py
@@ -10,18 +10,18 @@ class HydroModel ():
 
 
     def hydro_model_output_request(self, output_requested):
-        expected_potential = hydro_potential - output_requested # expected potential in reservoir - can be negative
-        hydro_potential = max(expected_potential, 0) # hydro potential is set to expected potential bound to 0+
+        expected_potential = self.hydro_potential - output_requested # expected potential in reservoir - can be negative
+        self.hydro_potential = max(expected_potential, 0) # hydro potential is set to expected potential bound to 0+
         output_actual = output_requested + min(expected_potential, 0) 
         # if the requested output cannot be fully provided, the potential that could not be generated is removed from the requested amount
 
 
     def hydro_model_remove_overflow ():
-        hydro_potential = min(hydro_potential, FULL_HYDRO_POTENTIAL)
+        self.hydro_potential = min(hydro_potential, FULL_HYDRO_POTENTIAL)
 
 
     def hydro_model_DC1(self, total_demand):
-        hydro_potential += input_potential # water flows in
+        self.hydro_potential += self.input_potential # water flows in
         return hydro_model_output_request(float(total_demand) * 0.20) # request 20% of demand
 
 
