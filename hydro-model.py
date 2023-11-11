0e9bd3cfb31d27988fdf187868f068dd0f0195c4
EchoDevG <garrattecho1@gmail.com>
2023-11-11 18:05:57 +0000

    added comments

--- hydro-model.py
+++ hydro-model.py
@@ -3,7 +3,7 @@
 # function takes total power into reservoirs, potential power stored in reservoirs, power out wanted by grid.
 # returns actual output power from dams
 
-class HydroModel ();
+class HydroModel ():
 
     def __init__(self, hydro_potential, in_power_MW, FULL_HYDRO_POTENTIAL):
         self.hydro_potential = hydro_potential
@@ -11,18 +11,29 @@ class HydroModel ();
         self.FULL_HYDRO_POTENTIAL = FULL_HYDRO_POTENTIAL
 
 
+
+
+
     def hydro_model_DC1(total_demand, timestep):
         
         out = total_demand * 0.2
 
+        # if hydro can provide enough power, give the power
         if (hydro_potential >= out):
             hydro_potential = hydro_potential - out
             out = out
         else:
+            # else, give what power it can and report an error
             out = hydro_potential
             hydro_potential = 0
             print(f"ERROR!!! AT {timestep}. HYDRO CANNOT PROVIDE ENOUGH POWER IN DC1")
         return (out)
+
+
+
+
+
+
 
     def hydro_model (in_power_MW, out_power_request):
 
