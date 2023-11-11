e02a71f4956756230c2c782fbd8a8f0980316db9
EchoDevG <garrattecho1@gmail.com>
2023-11-11 16:58:31 +0000

    updated hydro-dam model

--- hydro-model.py
+++ hydro-model.py
@@ -1,22 +1,32 @@
 
 
 # function takes total power into reservoirs, potential power stored in reservoirs, power out wanted by grid.
-# returns actual output power from dams, potential pwoer stored in reservoirs
+# returns actual output power from dams
 
 class HydroModel ();
 
-    def __init__():
+    def __init__(self, hydro_potential, in_power_MW, FULL_HYDRO_POTENTIAL):
+        self.hydro_potential = hydro_potential
+        self.in_power_MW = in_power_MW
+        self.FULL_HYDRO_POTENTIAL = FULL_HYDRO_POTENTIAL
 
-    def hydro_model (in_power_MW, hydro_potential, out_power_request):
-        FULL_HYDRO_POTENTIAL = 10_000_000
+    def hydro_model (in_power_MW, out_power_request):
 
-        if (hydro_potential == FULL_HYDRO_POTENTIAL):
+        total = hydro_potential + in_power_MW
+        out = 0
+
+        if (total > FULL_HYDRO_POTENTIAL):
+            out = max(out_power_request, in_power_MW)
+        elif ((hydro_potential-out_power_request) >= 0):
+            out = out_power_request
+        else:
+            out = total
+
+        hydro_potential = total - out
+        return (out)
 
 
 
-            return (out_power_MW, hydro_potential)
 
-        if (hydro_potential < 0.2 * FULL_HYDRO_POTENTIAL):
-            return(0)
-
+        final_total = total - out
 
