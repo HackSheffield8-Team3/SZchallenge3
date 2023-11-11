d2b1c4ea2b9c63edfa422901ad2f3544b56a6bec
bwsmith-dev <bwsmith161@gmail.com>
2023-11-11 21:58:26 +0000

    DC1 and DC3 have been refactored completely with a general request function.

--- hydro_model.py
+++ hydro_model.py
@@ -9,46 +9,21 @@ class HydroModel ():
         self.FULL_HYDRO_POTENTIAL = FULL_HYDRO_POTENTIAL
 
 
-
-
-
-    def hydro_model_DC1(self, total_demand):
-        """
-        func documentation
-        """
-        output_requested = float(total_demand) * 0.20 # 20% of demand
-        hydro_potential += input_potential # water flows in
+    def hydro_model_output_request(self, output_requested):
         expected_potential = hydro_potential - output_requested # expected potential in reservoir - can be negative
         hydro_potential = max(expected_potential, 0) # hydro potential is set to expected potential bound to 0+
         output_actual = output_requested + min(expected_potential, 0) 
         # if the requested output cannot be fully provided, the potential that could not be generated is removed from the requested amount
 
-        """
-        if (hydro_potential >= out): # give power 
-            hydro_potential -= out
-            # out = out ()
-        else:
-            # else, give what power it can and report an error
-            out = hydro_potential
-            hydro_potential = 0
-            print("ERROR: HYDRO CANNOT PROVIDE ENOUGH POWER IN DC1")
-        """
-        return output_actual
-
-
-
-
-
-
-
-
-    def hydro_model_DC3 (self, out_power_request):
-        """
-        func documentation
-        """
-        expected_potential = hydro_potential - output_requested
-    
-        # something
 
     def hydro_model_remove_overflow ():
         hydro_potential = min(hydro_potential, FULL_HYDRO_POTENTIAL)
+
+
+    def hydro_model_DC1(self, total_demand):
+        hydro_potential += input_potential # water flows in
+        return hydro_model_output_request(float(total_demand) * 0.20) # request 20% of demand
+
+
+    def hydro_model_DC3 (self, output_requested):
+        hydro_model_output_request(self, output_requested)
