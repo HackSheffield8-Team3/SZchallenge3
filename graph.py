51786276c6ea987b599d7bacdda26cbe37f48ff9
EchoDevG <garrattecho1@gmail.com>
2023-11-12 01:53:21 +0000

    updated initial plotting function

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-
+# pass this function an array of hydro_usage every time step, in MW.
 def plot_usage(hydro_data):
     plt.plot(hydro_data)
     plt.ylabel('hydro usage')
