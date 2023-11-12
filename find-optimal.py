8e9642d39b09b15f3de0a43f7530b99f7701aada
EchoDevG <garrattecho1@gmail.com>
2023-11-12 07:41:54 +0000

    first optimiser attempt

--- find-optimal.py
+++ find-optimal.py
@@ -1,23 +1,23 @@
 from model import EnergyGrid
+import scipy.optimize as optimize
 
-best_cost = 0
-best_ijk = [0,0,0]
-for i in range(1, 3):
-    for j in range(1, 3):
-        for k in range(1, 3):
-            grid = EnergyGrid(i,j,k, 999999999999)
-            grid = grid
-            
-            # assume grid.run_model() returns [fraction renewable, cost]
-            data = grid.run_model()
-            if data[0] >=0.98:
-                if data[1] <= best_cost:
-                    best_cost = data[1]
-                    best_ijk = [i,j,k]
-            #store the best i,j,k so far based on cost
-            #when done, output the stored value
+def func_to_optimise(args):
+    a,b,c = args
+    grid = EnergyGrid(1,3000,1000, 999999999)
+    grid = grid
+    data = grid.run_model()
+    if data[0] <=0.98:
+        return 9999999999999999999999999
+    else:
+        return data[1]
 
-print(str(best_cost))
-print(str(best_ijk[0]))
-print(str(best_ijk[1]))
-print(str(best_ijk[2]))
+initial_guess = [1, 500, 10000]
+
+result = optimize.minimize(func_to_optimise, initial_guess)
+
+
+if result.success:
+    fitted_params = result.x
+    print(fitted_params)
+else:
+    raise ValueError(result.message)
