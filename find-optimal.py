2c0bef0602a5157578b953574a40630b2e06acfd
EchoDevG <garrattecho1@gmail.com>
2023-11-12 06:27:31 +0000

    goofy code bro

--- find-optimal.py
+++ find-optimal.py
@@ -2,10 +2,10 @@ from model import EnergyGrid
 
 best_cost = 0
 best_ijk = [0,0,0]
-for i in range(1, 1000000):
-    for j in range(1, 1000000):
-        for k in range(1, 1000000):
-            grid = EnergyGrid(i,j,k)
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            grid = EnergyGrid(i,j,k, 999999999999)
             grid = grid
             
             # assume grid.run_model() returns [fraction renewable, cost]
@@ -16,3 +16,8 @@ for i in range(1, 1000000):
                     best_ijk = [i,j,k]
             #store the best i,j,k so far based on cost
             #when done, output the stored value
+
+print(str(best_cost))
+print(str(i))
+print(str(j))
+print(str(k))
