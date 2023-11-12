3df006ca1b11f3e97b13f1b8491027f3dd8514ce
EchoDevG <garrattecho1@gmail.com>
2023-11-12 06:32:54 +0000

    shit code

--- find-optimal.py
+++ find-optimal.py
@@ -2,9 +2,9 @@ from model import EnergyGrid
 
 best_cost = 0
 best_ijk = [0,0,0]
-for i in range(1, 10):
-    for j in range(1, 10):
-        for k in range(1, 10):
+for i in range(1, 3):
+    for j in range(1, 3):
+        for k in range(1, 3):
             grid = EnergyGrid(i,j,k, 999999999999)
             grid = grid
             
@@ -18,6 +18,6 @@ for i in range(1, 10):
             #when done, output the stored value
 
 print(str(best_cost))
-print(str(i))
-print(str(j))
-print(str(k))
+print(str(best_ijk[0]))
+print(str(best_ijk[1]))
+print(str(best_ijk[2]))
