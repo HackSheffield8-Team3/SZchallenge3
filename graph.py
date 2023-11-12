538923ec33df308d38efc813e3c1a12aa224af88
EchoDevG <garrattecho1@gmail.com>
2023-11-12 01:59:03 +0000

    stackplot code

--- graph.py
+++ graph.py
@@ -1,7 +1,22 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 # pass this function an array of hydro_usage every time step, in MW.
-def plot_usage(hydro_data):
-    plt.plot(hydro_data)
-    plt.ylabel('hydro usage')
+def plot_usage(timesteps, hydro_data, geo_data, solar_data):
+    timeframes = timeframes
+    power_by_type = {
+        'hydro': hydro_data,
+        'geo': geo_data,
+        'solar': solar_data,
+    }
+
+    fig, ax = plt.subplots()
+    ax.stackplot(timeframes, power_by_type.values(),
+                 labels=power_by_type.keys(), alpha=0.8)
+    ax.legend(loc='upper left', reverse=True)
+    ax.set_title('Power usage')
+    ax.set_xlabel('timestep')
+    ax.set_ylabel('Usage (MW30m)')
+
     plt.show()
