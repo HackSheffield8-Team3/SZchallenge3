f61aae46778f0aa24d1ec66f1069a533c79d1e29
Jason <git@thebowstones.net>
2023-11-12 03:54:40 +0000

    Wind model now handles battery storage

--- wind_model.py
+++ wind_model.py
@@ -1,12 +1,15 @@
 class WindModel ():
-    def wind_model(self, requested_output, actual_output, available_battery_storage_capacity):
+    def wind_model(self, requested_output, actual_output, available_storage_capacity):
         #if demand less than wind energy output, useful power is total requested output and leftover is remaining wind energy
         if requested_output < actual_output:
-            useful_power = requested_output
+            used = requested_output
             leftover_power = actual_output - useful_power
         #if demand greater than/equal to wind energy output, useful power is all wind energy at timestamp and leftover is zero
         else:
             useful_power = actual_output
             leftover_power = 0
+        
+        stored  = min(leftover_power, available_storage_capacity)
+        wasted = leftover_power - stored
 
-        return [useful_power, leftover_power]
+        return [used, stored, wasted]
